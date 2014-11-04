06f6e6cf8df48a24ba40c9356e84591cec9718b3
cp-imageupload <alextehkh@gmail.com>
2014-11-04 22:07:49 +0800

    adding image viewer in webroot
    adding image viewer in webroot folder

---
+++ app/webroot/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>PicUr</title>
+	<style type="text/css">
+	body {
+		background:#eee;
+		margin:0;
+		padding:0;
+		font:12px arial, Helvetica, sans-serif;
+		color:#222;
+	}
+	</style>
+	<link type="text/css" rel="stylesheet" href="plugin/album.css" />
+	<link type="text/css" rel="stylesheet" href="plugin/colorbox/colorbox.css" />
+	<script type="text/javascript" src="plugin/jquery.1.11.js"></script>
+	<script type="text/javascript" src="plugin/colorbox/jquery.colorbox-min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+    // initiate colorbox
+    $(".albumpix").colorbox({rel:'albumpix', maxWidth:'98%', maxHeight:'98%', slideshow:true, slideshowSpeed:3500, slideshowAuto:false});
+});	
+	</script>
+</head>
+<body>
+
+	<br />
+	<a href="../../addimage/display"> add image</a>
+	<br />
+
+	<?php $_REQUEST['fullalbum']=1; include 'g.php'; ?>
+
+
+
+
+</body>
+</html>
